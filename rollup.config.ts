e0ee35ebbf68e701541eed4a05cb4bde15b2f4fc
zhouhongxuan <zhouhongxuan@kuaishou.com>
2020-01-20 22:19:00 +0800

    WIP: build for browser

---
+++ rollup.config.ts
@@ -0,0 +1,63 @@
+import path from 'path';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve'; // resolve external packages
+import typescript from 'rollup-plugin-typescript2'; // notice ts version (https://github.com/ezolenko/rollup-plugin-typescript2/issues/88)
+import strip from '@rollup/plugin-strip';
+import { terser } from "rollup-plugin-terser";
+import pkg from './package.json';
+
+const ROOT_DIR = __dirname;
+const DIST_DIR = path.resolve(ROOT_DIR, 'dist', 'browser');
+const BANNER = `/** GIFrame.js v${pkg.version}, repo: https://github.com/alienzhou/giframe, MIT licence */`;
+
+export default {
+    input: path.resolve(ROOT_DIR, 'src', 'giframe.ts'),
+    plugins: [
+        typescript({
+            tsconfig: 'tsconfig.browser.json',
+        }),
+        resolve({
+            mainFields: ['module', 'main'],
+            browser: true
+        }),
+        commonjs(),
+        strip()
+    ],
+    output: [{
+        file: path.resolve(DIST_DIR, 'giframe.js'),
+        format: 'umd',
+        name: 'GIFrame',
+        sourcemap: true,
+        banner: BANNER,
+        globals: {
+            window: 'window'
+        },
+        plugins: [
+            babel({ runtimeHelpers: true }),
+            terser({
+                output: {
+                    comments: /GIFrame.js/,
+                }
+            })
+        ]
+    }, {
+        file: path.resolve(DIST_DIR, 'giframe.esm.js'),
+        format: 'esm',
+        name: 'GIFrame',
+        banner: BANNER,
+        sourcemap: true,
+        plugins: [
+            terser({
+                compress: false,
+                mangle: false,
+                module: true,
+                output: {
+                    beautify: true,
+                    comments: /GIFrame.js/,
+                    braces: true
+                }
+            })
+        ]
+    }]
+};
--- test/proxy.test.ts
+++ test/proxy.test.ts
@@ -1,10 +1,9 @@
 import { expect } from 'chai';
 import createArrayProxy from '../src/utils/proxy';
-import { BufferArray } from '../src/types';
 
 describe('Proxy', () => {
     it('should return val for a symbol key', function () {
-        const arr: BufferArray = Buffer.from([0, 1, 2]);
+        const arr: Uint8Array = Buffer.from([0, 1, 2]);
         const key = Symbol('hey');
         const val = 'hi';
         arr[key] = val;
@@ -13,7 +12,7 @@ describe('Proxy', () => {
     });
 
     it('should return val for a NaN string key', function () {
-        const arr: BufferArray = Buffer.from([0, 1, 2]);
+        const arr: Uint8Array = Buffer.from([0, 1, 2]);
         const key: string = 'hey';
         const val: string = 'hi';
         arr[key] = val;
@@ -22,14 +21,14 @@ describe('Proxy', () => {
     });
 
     it('should return val when numerical key is not out of range', function () {
-        const arr: BufferArray = Buffer.from([0, 1, 2]);
+        const arr: Uint8Array = Buffer.from([0, 1, 2]);
         const proxy = createArrayProxy(arr);
         const access = () => proxy[0] & proxy[1] & proxy[2];
         expect(access).not.to.throw();
     });
 
     it('should throw error when numerical key is out of range', function () {
-        const arr: BufferArray = Buffer.from([0, 1, 2]);
+        const arr: Uint8Array = Buffer.from([0, 1, 2]);
         let proxy = createArrayProxy(arr);
         const access = () => proxy[3];
 
@@ -38,7 +37,7 @@ describe('Proxy', () => {
     });
 
     it('should throw error when numerical-like string key is out of range', function () {
-        const arr: BufferArray = Buffer.from([0, 1, 2]);
+        const arr: Uint8Array = Buffer.from([0, 1, 2]);
         let proxy = createArrayProxy(arr);
         const access = () => proxy['3'];
 
